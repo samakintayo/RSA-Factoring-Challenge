3b3feee146cedd54d9bb4e52a3f08094ea69156b
Samkintayo <tayoakinnayajo@gmail.com>
2023-01-29 14:44:55 +0000

    fix

--- factors.c
+++ factors.c
@@ -15,23 +15,28 @@ int main(int argc, char *argv[])
 	long long flag = 1, div, rest, number, counter;
 	ssize_t nread;
 
-	if (argc != 2) {
+	if (argc != 2)
+	{
 		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
 	stream = fopen(argv[1], "r");
-	if (stream == NULL) {
+	if (stream == NULL)
+	{
 		perror("fopen");
 		exit(EXIT_FAILURE);
 	}
 
-	while ((nread = getline(&line, &len, stream)) != -1) {
+	while ((nread = getline(&line, &len, stream)) != -1)
+	{
 		flag = 1, div = 2;
 		number = atoll(line);
-		while (flag) {
+		while (flag)
+		{
 			rest = number % div;
-			if (!rest) {
+			if (!rest)
+			{
 				counter = number / div;
 				printf("%lld=%lld*%lld\n", number, counter, div);
 				flag = 0;
